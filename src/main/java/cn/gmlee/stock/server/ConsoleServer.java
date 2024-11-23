40f373a8047d004c2348291cfae320556bc9078c
Jason° <xiaoku13141@163.com>
2024-11-23 22:51:41 +0800

    控制台交互完成

--- src/main/java/cn/gmlee/stock/server/ConsoleServer.java
+++ src/main/java/cn/gmlee/stock/server/ConsoleServer.java
@@ -1,11 +1,13 @@
 package cn.gmlee.stock.server;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /**
  * The type Console server.
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ConsoleServer {
@@ -15,6 +17,6 @@ public class ConsoleServer {
      * @param content the content
      */
     public void handle(String content) {
-        System.out.println("检测到输入内容: " + content);
+        log.info("检测到输入内容: {}", content);
     }
 }
