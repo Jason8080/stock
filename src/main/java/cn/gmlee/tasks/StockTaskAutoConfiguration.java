28b1afd1318a83fe809e3b8eb438ab4338b4bb06
Jason° <xiaoku13141@163.com>
2024-11-23 20:29:15 +0800

    打包完成

--- src/main/java/cn/gmlee/tasks/StockTaskAutoConfiguration.java
+++ src/main/java/cn/gmlee/tasks/StockTaskAutoConfiguration.java
@@ -1,21 +1,39 @@
 package cn.gmlee.tasks;
 
 import cn.gmlee.stock.server.StockServer;
+import cn.gmlee.tools.base.util.BoolUtil;
+import cn.gmlee.tools.base.util.NullUtil;
+import cn.gmlee.tools.ds.dynamic.DynamicDataSourceHolder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class StockTaskAutoConfiguration {
 
     private final StockServer stockServer;
 
+    private String db = System.getProperty("db");
+
+    @PostConstruct
+    public void init(){
+        log.warn("当前持久化采用的是: {}", NullUtil.get(db, "mysql"));
+    }
+
     /**
      * 更新股票列表
      */
     @Scheduled(cron = "0 0 0 * * 6")
-    public void stockUpdate(){
+    public void stockUpdate() {
+        if (BoolUtil.notEmpty(db)) {
+            log.warn("当前持久化采用的是: {}", db);
+            DynamicDataSourceHolder.set(db);
+        }
         stockServer.stockUpdate();
     }
 
@@ -24,6 +42,10 @@ public class StockTaskAutoConfiguration {
      */
     @Scheduled(cron = "0 0 17 ? * 1-5")
     public void marketPull() {
+        if (BoolUtil.notEmpty(db)) {
+            log.warn("当前持久化采用的是: {}", db);
+            DynamicDataSourceHolder.set(db);
+        }
         stockServer.marketPull();
     }
 
@@ -32,6 +54,10 @@ public class StockTaskAutoConfiguration {
      */
     @Scheduled(cron = "0 0 18 ? * 1-5")
     public void dealHandle() {
+        if (BoolUtil.notEmpty(db)) {
+            log.warn("当前持久化采用的是: {}", db);
+            DynamicDataSourceHolder.set(db);
+        }
         stockServer.dealHandle();
     }
 
@@ -40,6 +66,10 @@ public class StockTaskAutoConfiguration {
      */
     @Scheduled(cron = "0 55 14 ? * 1-5")
     public void dealInform() {
+        if (BoolUtil.notEmpty(db)) {
+            log.warn("当前持久化采用的是: {}", db);
+            DynamicDataSourceHolder.set(db);
+        }
         stockServer.dealInform();
     }
 }
