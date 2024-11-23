62bb8c7be074bb35f857ff073647885008a3b9ec
Jason° <xiaoku13141@163.com>
2024-11-23 16:08:38 +0800

    添加基础代码

---
+++ src/main/java/cn/gmlee/stock/dao/entity/StockStrategy.java
@@ -0,0 +1,37 @@
+package cn.gmlee.stock.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.util.Date;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Jas°
+ * @since 2024-11-23
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("stock_strategy")
+public class StockStrategy implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String name;
+
+    private String author;
+
+    private Integer v;
+
+    private Integer status;
+
+    private Date createdAt;
+
+
+}
---
+++ src/main/java/cn/gmlee/stock/service/impl/StockStrategyServiceImpl.java
@@ -0,0 +1,19 @@
+package cn.gmlee.stock.service.impl;
+
+import cn.gmlee.stock.dao.entity.StockStrategy;
+import cn.gmlee.stock.dao.mapper.StockStrategyMapper;
+import cn.gmlee.stock.service.StockStrategyService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Jas°
+ * @since 2024-11-23
+ */
+@Service
+public class StockStrategyServiceImpl extends ServiceImpl<StockStrategyMapper, StockStrategy> implements StockStrategyService {
+}
