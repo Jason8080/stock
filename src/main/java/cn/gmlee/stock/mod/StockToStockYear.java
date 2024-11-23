66574be7af82f6b8409fb85e91bdfa903b06fc28
Jason° <xiaoku13141@163.com>
2024-11-23 18:10:07 +0800

    ..

--- src/main/java/cn/gmlee/stock/mod/StockToStockYear.java
+++ src/main/java/cn/gmlee/stock/mod/StockToStockYear.java
@@ -2,13 +2,27 @@ package cn.gmlee.stock.mod;
 
 
 import cn.gmlee.stock.dao.entity.Stock2024;
+import cn.gmlee.tools.base.util.TimeUtil;
 import org.mapstruct.Mapper;
+
+import java.util.Date;
+import java.util.Timer;
 
 /**
  * The interface Stock to stock year.
  */
 @Mapper(componentModel = "spring")
 public interface StockToStockYear {
+    /**
+     * String to date.
+     *
+     * @param timestamp the timestamp
+     * @return the date
+     */
+    default Date stringToDate(String timestamp) {
+        return TimeUtil.parseTime(timestamp);
+    }
+
     /**
      * To entity stock 2024.
      *
