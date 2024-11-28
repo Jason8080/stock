2d14ebaf2ccc14a2ffe938ef3ece6164505a7609
Jason° <xiaoku13141@163.com>
2024-11-28 21:00:58 +0800

    ..

---
+++ src/main/java/cn/gmlee/stock/dao/mapper/StockStrategyLockMapper.java
@@ -0,0 +1,16 @@
+package cn.gmlee.stock.dao.mapper;
+
+import cn.gmlee.stock.dao.entity.StockStrategyLock;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Jas°
+ * @since 2024-11-28
+ */
+public interface StockStrategyLockMapper extends BaseMapper<StockStrategyLock> {
+
+}
---
+++ src/main/java/cn/gmlee/stock/service/impl/StockStrategyLockServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.gmlee.stock.service.impl;
+
+import cn.gmlee.stock.dao.entity.StockStrategyLock;
+import cn.gmlee.stock.dao.mapper.StockStrategyLockMapper;
+import cn.gmlee.stock.service.StockStrategyLockService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Jas°
+ * @since 2024-11-28
+ */
+@Service
+public class StockStrategyLockServiceImpl extends ServiceImpl<StockStrategyLockMapper, StockStrategyLock> implements StockStrategyLockService {
+
+}
